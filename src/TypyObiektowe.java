43ebb64e7f62506f79396fce876e02af0413547a
Cirilla Tester <anna.poplawska0512@gmail.com>
2021-09-15 16:15:04 +0200

    commit typy zmiennych

---
+++ src/TypyObiektowe.java
@@ -0,0 +1,42 @@
+public class TypyObiektowe {
+
+    //ciągi znaków
+
+    String hello = "Hello Ania";
+
+    public static void main(String[] args) {
+        //liczby staloprzecinkowe
+
+        Byte firstNumber = 127;
+        Short secondNumber = 32689;
+        Integer thirdNumber = 32789567;
+        Long fourthNumber = 3982097L;
+
+        //liczny zmiennoprzecinkowe
+
+    Float fifthNumber = 4.99934F;
+    Double sixthNumber = 3.999999999999999D;
+
+    //wartość logiczna
+
+    Boolean prawda = true;
+    Boolean falsz = false;
+
+    //pojedynczy znak
+
+    Character letter = 'A';
+
+    //ciągi znaków
+
+        String hello = "Hello Ania";
+
+        System.out.println (hello.charAt(0));
+
+        //to wyzej jest po to zeby nam wyswietlil wartosci pierwszej w podanym ciagu "hello" zmiennej, czyli h
+
+
+
+
+    }
+
+}
---
+++ src/TypyProste.java
@@ -0,0 +1,30 @@
+public class TypyProste {
+    public static void main(String[] args) {
+        //liczby staloprzecinkowe
+
+        byte firstNumber = 127;
+        short secondNumber = 32689;
+        int thirdNumber = 32789567;
+        long fourthNumber = 3982097L;
+
+        //liczny zmiennoprzecinkowe
+
+        float fifthNumber = 4.99934F;
+        double sixthNumber = 3.999999999999999D;
+
+        //wartość logiczna
+
+        boolean prawda = true;
+        boolean falsz = false;
+
+        //pojedynczy znak
+
+        char letter = 'A';
+    }
+
+
+
+
+
+
+    }
