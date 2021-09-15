b706a246726f62bf15d70a2a342365c9997c9e92
Cirilla Tester <anna.poplawska0512@gmail.com>
2021-09-15 17:55:17 +0200

    commit zmienne2

---
+++ src/Zmienne.java
@@ -0,0 +1,21 @@
+//zarezerowane miejsce w pamieci, zmienna pojedyncza, z okreslonym typem danych (typ danych plus nazwa zmiennej)
+
+public class Zmienne {
+    String imie = "Ania nie Anieczka";
+
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+        int result;
+        String imie = "Ania nie Anieczka";
+
+        firstNumber =1;
+        secondNumber = 2;
+        result = firstNumber + secondNumber;
+        imie = " Katarzyna nie Kasia";
+        System.out.println(result);
+        System.out.println(imie);
+
+    }
+
+}
