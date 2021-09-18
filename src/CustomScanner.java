2651c04fabc715d7cb2d22a2d9f2aecb1bcc39a3
Cirilla Tester <anna.poplawska0512@gmail.com>
2021-09-18 13:10:39 +0200

    scanner commit

--- src/CustomScanner.java
+++ src/CustomScanner.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class CustomScanner {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
+        Scanner name = new Scanner(System.in); // tworzenie nowego obiektu klasy Scanner. Zmienna o nazwie name
+        System.out.println("Proszę, podaj swoje imię i naciśnij enter");
+        String Name = name.nextLine();
+        System.out.println(" hejka " + Name + " !");
+    }*/
 
+
+    public static void main(String[] args) {
+        Scanner cyfr = new Scanner(System.in);
+        System.out.println("Proszę, podaj pierwszą liczbę");
+        int Cyfra = cyfr.nextInt();
+        System.out.println("Kwadrat tej liczby to: " + Cyfra*Cyfra);
     }
 }
